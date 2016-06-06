8ba3daf0ee67d787b0a7348b9de4888abe58dac4
Adam Dye <adamdye@gmail.com>
2016-06-06 14:53:19 -0700

    add a lambda that uses an RxJava interface

--- mylibrary/src/main/java/adamdye/com/mylibrary/MyLibrary.java
+++ mylibrary/src/main/java/adamdye/com/mylibrary/MyLibrary.java
@@ -1,8 +1,18 @@
 package adamdye.com.mylibrary;
 
+import android.util.Log;
+
 import java.util.function.Function;
 
+import rx.Observable;
+
 public class MyLibrary {
+
+    public void methodThatUsesRxJava() {
+        Observable.just(1, 2, 3).subscribe(integer -> {
+            Log.i(MyLibrary.class.getName(), integer.toString());
+        });
+    }
 
     public void methodThatAcceptsLambda(Function<Object, Boolean> function) {
         function.apply(new Object());
